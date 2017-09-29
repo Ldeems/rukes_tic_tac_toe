f6af8ac6c5e03e4fb789db4cff43fc3653cfd512
Ldeems <luke.deems@yahoo.com>
2017-09-29 15:20:49 -0400

    added winner test6

--- test_board.rb
+++ test_board.rb
@@ -97,6 +97,15 @@ class Test_board < Minitest::Test
          assert_equal(true,board.winner)
      end
 
+     def test_board_winner_6
+        board = Board.new
+        player = "o"
+        board.updateboard(player,3)
+        board.updateboard(player,6)
+        board.updateboard(player,9)
+         assert_equal(true,board.winner)
+     end
+
    
 
     
