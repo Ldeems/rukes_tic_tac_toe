3c6fd251530c8038b04c0a20dee3453d21e6571c
Ldeems <luke.deems@yahoo.com>
2017-09-29 16:59:07 -0400

    added another player change test

--- player_class_test.rb
+++ player_class_test.rb
@@ -6,6 +6,13 @@ class Test_player < Minitest::Test
     def test_player_change_1
         player = Player.new
         assert_equal("x",player.playerchange)   
-    end    
+    end   
+    
+    def test_player_change_2
+        player = Player.new
+        player.playerchange
+    
+        assert_equal("o",player.playerchange)   
+    end 
 
 end    
