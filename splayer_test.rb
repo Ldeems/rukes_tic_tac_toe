c2c62ebb86eb1d18fe1964f3a4f0a4f45dbeeb8e
Ldeems <luke.deems@yahoo.com>
2017-09-29 19:55:08 -0400

    added another test for splayer

--- splayer_test.rb
+++ splayer_test.rb
@@ -6,6 +6,11 @@ class Test_splayer < Minitest::Test
     def test_splayer_1
         board = ["1","2","3","4","5","6","7","8","9"]
         assert_equal(0,Splayer.new.smove(board))
-    end    
+    end 
+    
+    def test_splayer_2
+        board = ["x","o","x","4","5","6","7","8","9"]
+        assert_equal(3,Splayer.new.smove(board))
+    end 
 end    
     
