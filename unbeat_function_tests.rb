da973a2259c38f74f61d783cd134cafb09ae5b19
Ldeems <luke.deems@yahoo.com>
2017-10-04 11:11:46 -0400

    first winning move func test

--- unbeat_function_tests.rb
+++ unbeat_function_tests.rb
@@ -1,11 +1,16 @@
 require 'minitest/autorun'
-# require_relative 'unwin.rb'
-# require_relative 'unblocker.rb'
+require_relative 'unwin.rb'
+require_relative 'unblocker.rb'
 
 class Test_unai < Minitest::Test 
 
     def test_practice_test
         assert_equal(6,6)
+    end 
+    
+    def test_winningmove_fucn_1
+        board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        assert_equal(false, winningmove(board))
     end    
 
 end    
--- unwin.rb
+++ unwin.rb
@@ -3,20 +3,24 @@ require_relative 'player_class.rb'
 require_relative 'unplayer.rb'
 require_relative 'unblocker.rb'
 
-def winningmove()
+def winningmove(board)
     player_class = Player.new
     board_class = Board.new
-    open_spaces = []
     board_class.gboard = board
-    board. each do |op|
-        if op == "x" || op == "o" 
-            
-        else
-            open_spaces << op
+    choice = ""
+    board. each_with_index do |op, index|
+        if op.to_i + 1 == index
+            board_class.updateboard(player_class,op)
+                if board_class.winner? == true
+                    choice = op
+                end
+            board_class.updateboard("#{index + 1}", "#{index + 1}")       
         end
-    end    
-        p open_spaces
-
+    end 
+    if choice == ""
+        false
+    else
+        choice
+    end           
 end
 
-winningmove()
