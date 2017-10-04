a7185992fbec7ecb933cf78c8a6c5d649a445d80
Ldeems <luke.deems@yahoo.com>
2017-10-04 10:37:26 -0400

    still breaking things up, got test page made and working with practice test

---
+++ unbeat_function_tests.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+# require_relative 'unwin.rb'
+# require_relative 'unblocker.rb'
+
+class Test_unai < Minitest::Test 
+
+    def test_practice_test
+        assert_equal(6,6)
+    end    
+
+end    
