f58bf15a628db34b805393e8ac1091dacb8c455e
Ldeems <luke.deems@yahoo.com>
2017-10-04 14:43:56 -0400

    made blocker function and its first test

--- unblocker.rb
+++ unblocker.rb
@@ -6,7 +6,31 @@ require_relative 'unwin.rb'
 
 
 
-def blockmove()
+def blockmove(board, player)
+    player_class = Player.new
+    player_class.player = player
+    board_class = Board.new
+    board_class.gboard = board
+    player_class.playerchange
+    player = player_class.player
 
+    choice = ""
+    board.each_with_index do |op, index|
+    #    p op
+    #    p index
+        if op == "#{index + 1}"
+            board_class.updateboard(player,op)
+                if board_class.winner? == true
+                    choice = op
+                end
+            board_class.updateboard("#{index + 1}", "#{index + 1}")       
+        end
+    end 
+    if choice == ""
+        false
+    else
+        choice
+    end        
+end
 
-end    
+  
