c086fd78b2283b8ee5e43b8f36e9076f2291963a
Ldeems <luke.deems@yahoo.com>
2017-10-05 17:55:38 -0400

    another test

--- unbeat_function_tests.rb
+++ unbeat_function_tests.rb
@@ -132,4 +132,9 @@ class Test_unai < Minitest::Test
         assert_equal("5", firstmove(board))
     end 
 
+    def test_firstmove_4
+        board = ["1", "2", "3", "o", "5", "6", "7", "8", "9"]
+        assert_equal("1", firstmove(board))
+    end 
+
 end    
